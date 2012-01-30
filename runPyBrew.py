4245f12e5f8040803e6e19f1d23f9b9acf2faea0
tgvoskuilen <tyler.voskuilen@gmail.com>
2012-01-29 21:10:11 -0500

    Updated

--- runPyBrew.py
+++ runPyBrew.py
@@ -1,63 +1,29 @@
-#----------------------------------------------------------------------
-# runPyBrew.py
-#
-# Created 10/28/2011
-#
-# Author: Tyler Voskuilen
-#
-#----------------------------------------------------------------------
+"""
+Copyright (c) 2012, Tyler Voskuilen
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met: 
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer. 
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution. 
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
 
 
-# TASKS (Pythonic, Bugs, and Features)
-# P   Update names (consistent case, Grain vs Ferm, etc...)
-# P   Improve documentation
-# B   Fix loading bugs in Windows 7
-# F   Add "Show Recipe" functionality to project
-# F   Add build from recipe option for projects
-#
-# B   Still some bugs with no projects... but hardly seems like a big issue
-# B   Fix icons in Windows
-
-# New/BeerSmith Features
-#  Timeline
-#   temperature/time graph ?
-#  Build project from recipe
-#  Show projects based on this recipe
-#  Boil-off calculations built in
-#   "Advanced Settings" panel
-#  Range bars to show position in ranges
-#  Add multiple yeasts to a project
-#  Design full-screen
-#  All ingredients together, count IBU of grains?
-#  Brew date calandar dropdown
-#  Export all/export selection
-#  Formatted export (to pdf for paper filing) or raw export (csv)
-#  Add a "tools" menu
-#  Add fermentation profiles, but don't lock projects to them
-#  Add an "Ingredients" view and don't hard-code ingredients any more
-#
-#
-# MAJOR TASKS
-#  1. Calander view
-#      Add more dates to each project (bottle time, etc)
-#      Add date dropdown on BrewDate entry
-#      Add calander view
-#      Get "event" icons for major events, mouseover or click for event text
-#
-#  2. Ingredients view (then styles view)
-#       Remove hard coding of ingredients and styles (pickle)
-#
-#  3. Multiple yeasts per project (combine attributes by averaging?)
-#
-#  4. Make "fancy" range bars for all ranges
-#
-#  5. Add more to "about" menu  
-#
-#
-# BUG LIST
-#  Double clicking on a project or recipe doesn't load it (load data not called)
-#
-#
 
 import wx
 import pyBrew
